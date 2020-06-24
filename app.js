9661cef84b65bf9cd3b66e185068a8f39c5b975b
bryndonhumphrey <64496396+bryndonhumphrey@users.noreply.github.com>
2020-06-24 12:45:52 -0700

    canvas resizing and mobile prompt

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ var express = require("express");
 
 var app = express();
 
-app.use(express.static("./"))
+app.use(express.static("./"));
 
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res){
-    res.render("Circles");
+app.get("/", function (req, res) {
+  res.render("Circles");
 });
 
-app.get("*", function(req, res){
-    res.render("Circles");
+app.get("*", function (req, res) {
+  res.render("Circles");
 });
 
 const PORT = process.env.PORT || 5000;
